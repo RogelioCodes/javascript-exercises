dd47deaad92e738cb2df3a77b209372718d1d20c
RogelioCodes <droozycraft@gmail.com>
2021-02-09 12:02:44 -0800

    completed findOldest and getTheTitles

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(array) {
+return array.reduce((oldest,current)=>{
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+return oldestAge > currentAge ? oldest : current ; 
+})
 }
 
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+
+}
 module.exports = findTheOldest
--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    console.log(array.map(book => book.author))
 
+    return array.map(book => book.title)
 }
 
 module.exports = getTheTitles;
